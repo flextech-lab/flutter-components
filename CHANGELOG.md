ac59e45617bf4ab12cd1f7f946483aa63e47e170
RafaelToebe <rafaeltoebe@gmail.com>
2021-03-26 15:07:16 -0300

    change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.1] - TODO: Add release date.
+## [0.0.1] - Inputs
 
-* TODO: Describe initial release.
+Adicionado TextInput, NumberInput e TextFormInput
--- lib/flextech.dart
+++ lib/flextech.dart
@@ -1,7 +1,5 @@
 library flextech;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'inputs/number_input/number_input.dart';
+export 'inputs/text_input/text_form_input.dart';
+export 'inputs/text_input/text_input.dart';
